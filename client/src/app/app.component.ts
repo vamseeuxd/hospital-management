81623d8c7f38f92f7f6c314100bfd60b9ec7dc70
vamseeuxd <vamsee.uxd@gmail.com>
2022-02-28 14:01:33 +0530

    Done with Issue fixes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'hospital-management';
+
+  constructor(public authService: AuthService) {
+    /*this.authService
+      .register({
+        firstName: 'Vamsee Kalyan',
+        lastName: 'Sunkara',
+        email: 'vamsi.flex@gmail.com',
+        password: '12345',
+      })
+      .subscribe(
+        (value: IRegisterResponse) => {
+          debugger;
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.error.error);
+        }
+      );*/
+    /*this.authService.login('vamsi.flex@gmail.com', '12345').subscribe(
+      (res: IRegisterResponse) => {
+        // debugger;
+        localStorage.setItem('token', res.token);
+        this.authService.user().subscribe((value1: any) => {
+          debugger;
+        });
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.error.error);
+      }
+    );*/
+
+    this.authService.user().subscribe((value1: any) => {
+      debugger;
+    });
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { TokenInterceptorService } from './services/token-interceptor/token-interceptor.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
