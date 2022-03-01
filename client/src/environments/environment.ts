eb48c53ec33512038095810b06aef998d4e6f608
vamseeuxd <vamsee.uxd@gmail.com>
2022-03-01 08:47:33 +0530

    Angular Upgrad Step-4

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,10 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  apiPath: 'http://localhost:3000/api/',
+  apiUrl: "http://localhost:4200",
 };
 
 /*
